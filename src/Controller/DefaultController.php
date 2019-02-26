0395797b197de6dd80c79e3808f172a65c7b68f3
JoseAlberto218 <josealberto.stf@gmail.com>
2019-02-26 20:27:48 +0100

    mi perfil y retoques varios

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -5,6 +5,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\Security\Core\Security;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use App\Entity\User;
 /**
  * @Route("/")
  */
@@ -17,12 +18,29 @@ class DefaultController extends Controller
     {
     	$session=$request->getSession()->get(Security::LAST_USERNAME);
     	if (isset($session)){
+    		$repository = $this->getDoctrine()->getRepository(User::class);
+    		$user = $repository->findOneByEmail($session);
     		$em = $this->getDoctrine()->getManager();
-        	return $this->render('default/indexLogged.html.twig');
+        	return $this->render('default/indexLogged.html.twig', [
+        	'controller_name'=>'DefaultController',
+        	'usuario' => $user]);
     	}
         $em = $this->getDoctrine()->getManager();
         return $this->render('default/index.html.twig');
     }
 
+    /**
+     * @Route("/profile", name="profile")
+     */
+    public function profile(Request $request): Response
+    {
+    	$session=$request->getSession()->get(Security::LAST_USERNAME);
+    	$repository = $this->getDoctrine()->getRepository(User::class);
+        $user = $repository->findOneByEmail($session);
+        $em = $this->getDoctrine()->getManager();
+        return $this->render('default/infoUser.html.twig', [
+        	'controller_name'=>'DefaultController',
+        	'usuario' => $user]);
+    }
     
 }
